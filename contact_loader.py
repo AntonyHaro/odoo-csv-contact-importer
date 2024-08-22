b385788da41c027abf0e82056fbc7a2e90cd9b79
AntonyHaro <antonyhpro@outlook.com>
2024-08-22 20:21:37 -0300

    adição do recurso de busca do country id baseado no nome do país

--- contact_loader.py
+++ contact_loader.py
@@ -21,8 +21,7 @@ def import_csv_contacts(file_name):
                 "name": header.index("name"),
                 "is_company": header.index("is_company"),
                 "company_name": header.index("company_name"),
-                # "country_id": header.index("country_id"),
-                # "state_id": header.index("state_id"),
+                "country_name": header.index("country_name"),
                 "zip": header.index("zip"),
                 "city": header.index("city"),
                 "street": header.index("street"),
@@ -42,8 +41,7 @@ def import_csv_contacts(file_name):
                     "name": row[column_indices["name"]].strip(),
                     "is_company": row[column_indices["is_company"]].strip(),
                     "company_name": row[column_indices["company_name"]].strip(),
-                    # "country_id": row[column_indices["country_id"]].strip(),
-                    # "state_id": row[column_indices["state_id"]].strip(),
+                    "country_id": row[column_indices["country_name"]].strip(),
                     "zip": row[column_indices["zip"]].strip(),
                     "city": row[column_indices["city"]].strip(),
                     "street": row[column_indices["street"]].strip(),
@@ -75,14 +73,16 @@ def authenticate(url, db, username, password):
         common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
         uid = common.authenticate(db, username, password, {})
 
-        if uid:
-            print(f"Autenticação feita com sucesso. UID: {uid}")
-        else: 
+        if not uid: 
             raise ValueError("Falha na autenticação. Verifique as credenciais.")
         return uid
     
     except Exception as e:
         print(f"Erro ao autenticar: {e}")
+
+def get_country_id(models, db, uid, password, country_name):
+    country_ids = models.execute_kw(db, uid, password, "res.country", "search", [[('name', '=', country_name)]])
+    return country_ids[0] if country_ids else None
 
 def create_contacts(url, db, uid, password, contacts):
     try: 
@@ -92,7 +92,15 @@ def create_contacts(url, db, uid, password, contacts):
             if not contact.get("name") or not contact.get("email"):
                 print(f"Contato inválido, faltando nome ou email: {contact}")
                 continue
-            
+
+            country_id = get_country_id(models, db, uid, password, contact["country_id"])
+
+            if country_id:
+                contact["country_id"] = country_id
+            else:
+                print(f"País '{contact['country_id']}' não encontrado. O contato não será criado.")
+                continue
+
             contact_id = models.execute_kw(db, uid, password, "res.partner", "create", [contact])
             print(f"Contato criado com o ID: {contact_id}")
         
